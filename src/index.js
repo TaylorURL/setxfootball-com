07c1a1521c2e6b812e7f302f85166a35d63b32e5
Sunday <trenton.taylor.email@gmail.com>
2026-06-13 16:41:35 -0500

    sunday: Write src/index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 /**
- * Application entry point. Mounts the root React component into the DOM
- * and reports web vitals.
+ * Application entry point. Mounts the root React component into the DOM,
+ * wraps it in the design-system ThemeProvider (dark by default), mounts the
+ * global Toaster, and reports web vitals.
  */
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { ThemeProvider, Toaster } from "@bradley-t-t/sunday-design-system";
 import "./index.css";
 import App from "./app/App";
 import reportWebVitals from "./hooks/reportWebVitals";
@@ -13,7 +15,10 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <ErrorBoundary>
-      <App />
+      <ThemeProvider defaultTheme="dark">
+        <App />
+        <Toaster position="bottom-right" />
+      </ThemeProvider>
     </ErrorBoundary>
   </React.StrictMode>,
 );
